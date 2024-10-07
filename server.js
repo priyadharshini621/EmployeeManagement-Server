60236e84badf28d6f3a5cfe5924ead394350c7a5
priyadharshini621 <ppriyadharshini621@gmail.com>
2024-10-07 12:27:36 +0530

    update cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(exp.json());
 
 //  app.use(cookie());
  app.use(cors({
-     origin:"http://localhost:5173",
+     origin:"https://radical-emp.netlify.app/",
     methods:["GET","PUT","POST","PATCH","DELETE"],
     credentials:true,
 
